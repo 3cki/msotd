04b104849b6d1966d043720b7e814053f83b70a6
Maximilian Eckert <eckertm2000@gmail.com>
2023-07-23 23:42:51 +0200

    4: fix: removed lint script

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,21 +8,18 @@ import {
   updateAndAddShortcuts,
 } from "./firestore";
 
-exports.readShortcuts = scheduler.onSchedule(
-  "every day at 00:00",
-  async (context) => {
-    try {
-      const url = getURLFromEnvs();
-      const html = await getHTMLFromURL(url);
-      const shortcuts = getShortcutsFromHTML(html);
-      const storedShortcuts = await getShortcutsFromFirestore();
-      const filteredShortcuts = removeIdenticalShortcuts(
-        shortcuts,
-        storedShortcuts
-      );
-      await updateAndAddShortcuts(filteredShortcuts);
-    } catch (error) {
-      logger.error(error);
-    }
+exports.readShortcuts = scheduler.onSchedule("* * * * *", async (context) => {
+  try {
+    const url = getURLFromEnvs();
+    const html = await getHTMLFromURL(url);
+    const shortcuts = getShortcutsFromHTML(html);
+    const storedShortcuts = await getShortcutsFromFirestore();
+    const filteredShortcuts = removeIdenticalShortcuts(
+      shortcuts,
+      storedShortcuts
+    );
+    await updateAndAddShortcuts(filteredShortcuts);
+  } catch (error) {
+    logger.error(error);
   }
-);
+});
